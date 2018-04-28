1e335765cd065307af73c2fb731e0947fcfcd00a
squ1dd132 <38809065+squ1dd132@users.noreply.github.com>
2018-04-28 11:34:54 +0100

    Add files via upload

--- import.sh
+++ import.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dpkg-deb -b ./debs/com.squ1dd13.redditrandom_0.0.1-9+debug_iphoneos-arm.deb
+
 dpkg-scanpackages -m ./ /dev/null | gzip > Packages.gz;
 
 dpkg-scanpackages -m ./ /dev/null | bzip2 > Packages.bz2;
